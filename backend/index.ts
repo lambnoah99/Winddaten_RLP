1e5ea8c55429bc754541229fa1e82aca00050b1a
lambnoah99 <n.lambert2@web.de>
2022-09-09 23:21:50 +0200

    Backend: FUNC-API-010

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,36 @@
-import express, { Request, Response} from 'express';
+import *as express from 'express';
+import { Request, Response} from 'express';
+import { createConnection } from 'mysql2';
 
 const PORT: number = 8081;
 // Instantiate new Epxress-Server
 const app = express();
+app.use(express.json());
+
+// Db Config
+const connection = createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '12345678',
+    database: 'wind'
+});
+// Connect to DB
+connection.connect(); 
 
 app.get('/', (_req: Request, res: Response) => {
     res.send("Hello World");
+})
+
+app.get('/anlagen', (_req: Request, res: Response) => {
+    connection.query('SELECT * FROM anlagen', (err, rows) => {
+        if(err) {
+            console.error(err);
+        }
+        res.json(rows);
+    });
 });
+
 
 // Start Epxress Server 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
