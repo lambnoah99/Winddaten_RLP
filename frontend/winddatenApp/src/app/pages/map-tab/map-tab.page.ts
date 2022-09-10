0039276b203785ce961777a79d557081ebbc5672
lambnoah99 <n.lambert2@web.de>
2022-09-10 20:36:00 +0200

    Frontend: FUNC-MAP-020

--- frontend/winddatenApp/src/app/pages/map-tab/map-tab.page.ts
+++ frontend/winddatenApp/src/app/pages/map-tab/map-tab.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Leaflet from 'leaflet';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-map-tab',
@@ -14,12 +15,33 @@ export class MapTabPage {
   }
 
   initMap() {
-    this.map = new Leaflet.Map('map').setView([49.84847790390487, 7.376896872850395], 8.7);
+    // Instantiate new Map with RLP in View
+    this.map = new Leaflet.Map('map').setView([49.84847790390487, 7.376896872850395], 8.8);
 
-    Leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      attribution: ''
-    }).addTo(this.map);
+    // Add OpenStreetMap as a Layer
+    Leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
+  
+    // Get User Coordinates and add a Marker to the Map
+    this.getCoordinates()
+    .then((coordinates) => {
+      const icon = Leaflet.icon({
+        iconUrl: 'assets/marker-icon-2x.png',
+        iconSize: [15, 24],
+        riseOnHover: true
+      });
+      this.addMarker([coordinates.latitude, coordinates.longitude], { icon });
+    })
+  }
 
+
+  // Adds a Marker to the Map
+  private addMarker(coordinates: number[], options?: Object) {
+    Leaflet.marker(coordinates, options).addTo(this.map);
+  }
+
+  // Returns Coordinates of Device
+  private async getCoordinates() {
+    return await (await Geolocation.getCurrentPosition()).coords;
   }
 
   ionViewWillLeave() {
